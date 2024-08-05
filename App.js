a2fd18ed0612adf8847d09051bf906b6585b3d91
Scott Martínez <scottmtinez@gmail.com>
2024-08-04 20:45:47 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
-Test
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>[PUMP] <span className='header-sub-title'>- Weightlifting Tracker</span></header>
+      <Home />
+      
+      <footer className='footer'>Workout Tracker Web Application | Portfolio Project | 2024</footer>
+    </div>
+  );
+}
+
+export default App;
