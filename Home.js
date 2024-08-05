d248ee303747e959a5660184fecbc4935ac2ec16
Scott Martínez <scottmtinez@gmail.com>
2024-08-04 20:56:29 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,1 +1,15 @@
-Test
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+
+
+function Home(){
+    return (
+        <div className='home-background'>
+            <div className='home-main-container'>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
