879c062a64ed75b020587a0a59fe8d2341eced98
Scott Martínez <scottmtinez@gmail.com>
2024-08-07 11:14:24 -0500

    Update App.js
    Will be updated with routes and links

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import React from 'react';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className='header'>[PUMP] <span className='header-sub-title'>- Weightlifting Tracker</span></header>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>HOME</a>
+          </li>
+          <li>
+            <a href='/'>MY WORKOUT</a>
+          </li>
+          <li>
+            <a href='/'>EXERCISES</a>
+          </li>
+          <li>
+            <a href='/'>ACCOUNT</a>
+          </li>
+        </ul>
+      </nav>
+
       <Home />
       
       <footer className='footer'>Workout Tracker Web Application | Portfolio Project | 2024</footer>
